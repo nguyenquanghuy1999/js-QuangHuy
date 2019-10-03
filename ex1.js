e132f50296b88ea18eac1ef01ce4a61dceb93a57
nguyenquanghuy1999 <nguyenquanghuy1999@gmail.com>
2019-10-03 11:04:25 +0700

    Hello

---
+++ ex1.js
@@ -0,0 +1,11 @@
+// Tim hieu ve left va const
+// Tu khoa dung de khai bao bien
+let s;
+s = "toi la huy";
+console.log(s);
+s = "toi la Beo ";
+console.log(s);
+
+const sv= "Nguyen quang huy";
+console.log(sv);
+
